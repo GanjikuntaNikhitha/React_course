27aa479d50455b2315ac0cc32954ca488d7cbf43
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-12-21 13:14:27 +0530

    Creating Functional Component with example

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person.js';
 class App extends Component {
   render() {
-    //return (
-    //  <div className="App">
-    //  <h1>I am a react App</h1>
-    //  </div>
-    //);
+    return (
+      <div className="App">
+            <h1>I am a react App</h1>
+            <Person></Person>
+      </div>
+    );
       //the above code javascript code converts into jsx as shown below
 
       //return React.createElement('div', null, 'h1', 'I am a react App')
       //the above code wont work it will gives output as hiI am a react App
 
-      return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does it works??'));
+      //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does it works??'));
       //the below code works and gives outout as Does it works?? the above examples are for understanding of jsx
 
 
---
+++ my-react-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// we dont need component here becz creating function
+// we used to import for converting jsx 
+const person = () => {
+    return <p>I'm a person!</p>
+}
+export default person;
