a83109878784f3f2916ee56f9d8c0709c247d852
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-12-21 15:41:21 +0530

    Dynamic content

--- my-react-app/src/Person/Person.js
+++ my-react-app/src/Person/Person.js
@@ -2,6 +2,7 @@ import React from 'react';
 // we dont need component here becz creating function
 // we used to import for converting jsx 
 const person = () => {
-    return <p>I'm a person!</p>
+    return <p>I'm a person! and I am {Math.floor(Math.random() * 30)}</p>
+    //Dynamic representation  {Math.floor(Math.random() * 30)}
 }
 export default person;
