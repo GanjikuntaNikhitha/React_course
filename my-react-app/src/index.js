b488d8ea27960d51070e79ba4399520c27d5c4e0
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-12-21 11:43:49 +0530

    Rendering HTML and App.js component.

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -5,4 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+//we are not rendering the app component we are just rendering a normal HTML element.
+
 registerServiceWorker();
